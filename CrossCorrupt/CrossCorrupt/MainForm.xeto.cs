f445911588e9b282c93e5f141c024ecc64aee275
Ravbug <ravbuganimations@gmail.com>
2018-11-25 12:39:17 -0800

    UI changes
    MainForm.xeto
    + controls without names that need names now have names
    ~ minor changes
    
    MainForm.xeto.cs
    + control defaults initialized
    + Corrupt Type dropdown has effect
    ~ moved all the UI handlers to the bottom of the file

--- CrossCorrupt/CrossCorrupt/MainForm.xeto.cs
+++ CrossCorrupt/CrossCorrupt/MainForm.xeto.cs
@@ -19,6 +19,10 @@ namespace CrossCorrupt
         private NumericStepper oldByteStepper;
         private NumericStepper newByteStepper;
         private CheckBox autoEndCheck;
+        private DropDown CorruptTypeCombo;
+        private GroupBox FolderCorruptBox;
+        private GroupBox FolderScrambleBox;
+        private CheckBox EnableFolderScrambleChck;
 
         private string[] sourceFiles;
 
@@ -27,37 +31,7 @@ namespace CrossCorrupt
 
             XamlReader.Load(this);
             SelectTypeList.SelectedIndex = 0;
-        }
-
-        //when the Auto End byte is clicked
-        protected void handleAutoEndClick(object sender, EventArgs e)
-        {
-            if ((bool)autoEndCheck.Checked)
-            {
-                endBytesStepper.Value = -1;
-            }
-            else
-            {
-                endBytesStepper.Value = 100;
-            }
-            endBytesStepper.Enabled = !(bool)autoEndCheck.Checked;
-        }
-
-        //some sample methods to delete later
-        protected void HandleClickMe(object sender, EventArgs e)
-        {
-            MessageBox.Show("I was clicked!");
-        }
-
-        //but keep these
-        protected void HandleAbout(object sender, EventArgs e)
-        {
-            new AboutDialog().ShowDialog(this);
-        }
-
-        protected void HandleQuit(object sender, EventArgs e)
-        {
-            Application.Instance.Quit();
+            CorruptTypeCombo.SelectedIndex = 0;
         }
 
         /// <summary>
@@ -71,14 +45,15 @@ namespace CrossCorrupt
             if (InfileTxt.Text.Trim().Length > 0 && OutfileTxt.Text.Trim().Length > 0)
             {
                 CorruptManager cm;
+                CorruptManager.CorruptionType type = (CorruptManager.CorruptionType)CorruptTypeCombo.SelectedIndex;
                 if (SelectTypeList.SelectedIndex == 0)
                 {
-                    cm = new CorruptManager(sourceFiles, OutfileTxt.Text, CorruptManager.CorruptionType.Replace, (long)startByteStepper.Value, (long)endBytesStepper.Value, (int)nBytesStepper.Value, (byte)oldByteStepper.Value, (byte)newByteStepper.Value);
+                    cm = new CorruptManager(sourceFiles, OutfileTxt.Text, type, (long)startByteStepper.Value, (long)endBytesStepper.Value, (int)nBytesStepper.Value, (byte)oldByteStepper.Value, (byte)newByteStepper.Value);
 
                 }
                 else
                 {
-                    cm = new CorruptManager(InfileTxt.Text, OutfileTxt.Text, CorruptManager.CorruptionType.Replace, (long)startByteStepper.Value, (long)endBytesStepper.Value, (int)nBytesStepper.Value, (byte)oldByteStepper.Value, (byte)newByteStepper.Value);
+                    cm = new CorruptManager(InfileTxt.Text, OutfileTxt.Text, type, (long)startByteStepper.Value, (long)endBytesStepper.Value, (int)nBytesStepper.Value, (byte)oldByteStepper.Value, (byte)newByteStepper.Value);
                 }
 
                 cm.Run((double prog, System.IO.FileInfo f) =>
@@ -165,6 +140,70 @@ namespace CrossCorrupt
             {
                 return null;
             }
+        }
+
+        //------------------------------------------------------ UI click handlers go below here ------------------------------------------------------
+
+        //when the Auto End byte is clicked
+        protected void handleAutoEndClick(object sender, EventArgs e)
+        {
+            if ((bool)autoEndCheck.Checked)
+            {
+                endBytesStepper.Value = -1;
+            }
+            else
+            {
+                endBytesStepper.Value = 100;
+            }
+            endBytesStepper.Enabled = !(bool)autoEndCheck.Checked;
+        }
+
+        //when the selection mode changed
+        protected void corruptTypeClicked(object sender, EventArgs e)
+        {
+            if (SelectTypeList.SelectedIndex ==1)
+            {
+                FolderCorruptBox.Enabled = true;
+                if (EnableFolderScrambleChck.Checked == true)
+                {
+                    FolderScrambleBox.Enabled = true;
+                }
+            }
+            else
+            {
+                FolderCorruptBox.Enabled = false;
+                FolderScrambleBox.Enabled = false;
+            }
+        }
+
+        //when folder scramble is enabled or disabled
+        protected void enableFolderScrambleStateChanged(object sender,EventArgs e)
+        {
+            if (EnableFolderScrambleChck.Checked == true && SelectTypeList.SelectedIndex == 1)
+            {
+                FolderScrambleBox.Enabled = true;
+            }
+            else
+            {
+                FolderScrambleBox.Enabled = false;
+            }
+        }
+
+        //some sample methods to delete later
+        protected void HandleClickMe(object sender, EventArgs e)
+        {
+            MessageBox.Show("I was clicked!");
+        }
+
+        //but keep these
+        protected void HandleAbout(object sender, EventArgs e)
+        {
+            new AboutDialog().ShowDialog(this);
+        }
+
+        protected void HandleQuit(object sender, EventArgs e)
+        {
+            Application.Instance.Quit();
         }
     }
 }
