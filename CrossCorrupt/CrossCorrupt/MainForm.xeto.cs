43af62140098afbe6abfc868026a10d11b0682fb
rosh324 <rosh324@users.noreply.github.com>
2019-01-15 14:38:20 -0800

    yeet

--- CrossCorrupt/CrossCorrupt/MainForm.xeto.cs
+++ CrossCorrupt/CrossCorrupt/MainForm.xeto.cs
@@ -396,16 +396,6 @@ namespace CrossCorrupt
         }
 
         /// <summary>
-        /// Clears the log view.
-        /// </summary>
-        /// <param name="sender">Sender.</param>
-        /// <param name="e">E.</param>
-        protected void ClearLogClicked(object sender, EventArgs e)
-        {
-            ConsoleLog.Text = "";
-        }
-
-        /// <summary>
         /// Copies the log to the clipboard.
         /// </summary>
         /// <param name="sender">Sender.</param>
@@ -416,7 +406,6 @@ namespace CrossCorrupt
         }
 
 
-       
 
         //but keep these
         protected void HandleAbout(object sender, EventArgs e)
